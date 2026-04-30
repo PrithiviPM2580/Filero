ebea58d68b5acb0539a4008084939e21290cfbef
Prithivi <prithivi@users.noreply.github.com>
2026-04-30 20:22:29 +0545

    feat: add TablesDB import and initialization in appwrite.ts

--- apps/api/src/lib/appwrite.ts
+++ apps/api/src/lib/appwrite.ts
@@ -1,4 +1,11 @@
-import { Client, Account, Databases, Storage, Avatars } from "node-appwrite"
+import {
+  Client,
+  Account,
+  Databases,
+  Storage,
+  Avatars,
+  TablesDB,
+} from "node-appwrite"
 import ENV from "@/config/env"
 
 const client = new Client()
@@ -8,5 +15,6 @@ const client = new Client()
 
 export const account = new Account(client)
 export const databases = new Databases(client)
+export const tablesDB = new TablesDB(client)
 export const storage = new Storage(client)
 export const avatars = new Avatars(client)
