32a330eb1f1570ec4301a47f88694d11c50b090a
Prithivi <prithivi@users.noreply.github.com>
2026-04-30 20:22:35 +0545

    feat: implement user registration and OTP email sending functionality

--- apps/api/src/controllers/auth.ts
+++ apps/api/src/controllers/auth.ts
@@ -1,3 +1,32 @@
+import { AppError } from "@/lib/error"
+import { createUser, getUserByEmail, sendEmailOTP } from "@/services/auth"
+import { RegisterType } from "@/validators/auth"
 import type { Request, Response, NextFunction } from "express"
 
-export const register = (req: Request, res: Response, next: NextFunction) => {}
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, fullName } = req.body as RegisterType
+
+  const existingUser = await getUserByEmail(email)
+
+  const accountId = await sendEmailOTP({ email })
+
+  if (!accountId) throw new AppError("Failed to send OTP email", 500)
+
+  if (!existingUser) {
+    await createUser({
+      email,
+      fullName,
+      accountId,
+    })
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "OTP sent to email",
+    accountId,
+  })
+}
